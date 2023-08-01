7926f5099aeac15ab90f34bc8deda6275e28cd70
Dimitre <dimitre@users.noreply.github.com>
2023-08-01 00:54:47 -0300

    up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,36 +3,36 @@
 using namespace ps3eye;
 
 void ofApp::setup(){
-	// ofSetFrameRate(0);
-	// ofSetVerticalSync(false);
-	// pixels.allocate(camDimensions.x, camDimensions.y, OF_IMAGE_COLOR);
-	// unsigned char * videoFrame [CAMW * CAMH * 3];
-	// const auto & devices( PS3EYECam::getDevices() );
+	ofSetFrameRate(0);
+	ofSetVerticalSync(false);
+	pixels.allocate(camDimensions.x, camDimensions.y, OF_IMAGE_COLOR);
+	unsigned char * videoFrame [CAMW * CAMH * 3];
+	const auto & devices( PS3EYECam::getDevices() );
 	
-	// cout << "devices size:  " << devices.size() << endl;
-	// if(devices.size()) {
+	cout << "devices size:  " << devices.size() << endl;
+	if(devices.size()) {
 		
-	// 	for (const auto & device : devices) {
-	// 		eye = device;
-	// 		bool ok = eye->init(camDimensions.x, camDimensions.y, FPS, PS3EYECam::EOutputFormat::RGB);
-	// 		if (ok) {
-	// 			videoTexture.allocate(camDimensions.x, camDimensions.y, GL_RGB);
-	// 			eye->start();
-	// 		} else {
-	// 		}
-	// 	}
-	// }
+		for (const auto & device : devices) {
+			eye = device;
+			bool ok = eye->init(camDimensions.x, camDimensions.y, FPS, PS3EYECam::EOutputFormat::RGB);
+			if (ok) {
+				videoTexture.allocate(camDimensions.x, camDimensions.y, GL_RGB);
+				eye->start();
+			} else {
+			}
+		}
+	}
 }
 
 void ofApp::update(){
-	// if (eye) {
-	// 	eye->getFrame(pixels.getData());
-	// }
+	if (eye) {
+		eye->getFrame(pixels.getData());
+	}
 }
 
 void ofApp::draw(){
-	// videoTexture.loadData(pixels);
-	// videoTexture.draw(0,0,eye->getWidth(),eye->getHeight());
+	videoTexture.loadData(pixels);
+	videoTexture.draw(0,0,eye->getWidth(),eye->getHeight());
 }
 
 void ofApp::keyPressed(int key) {
